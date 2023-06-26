de21113be9d5e3b98e2dc755e41c726d38f0bdb6
ANUKEERTHANA-S <anukeerthana.s@technotackle.com>
2023-06-26 13:28:47 +0530

    - Textinput

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import { Pressable, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, Text, TextInput, View, StyleSheet, Alert } from 'react-native';
 import { useConnection } from '@sendbird/uikit-react-native';
 
 const SignInScreen = () => {
     const { connect } = useConnection();
+    const [userId, setUserId] = useState();
+    const [nickName, setNickName] = useState();
+    const [token, setToken] = useState()
+
+    const signin = async () => {
+        if (userId == '') {
+            Alert.alert('Enter your useid');
+        } else if (nickName == '') {
+            Alert.alert('Enter your nickname');
+        } else if (nickName.length >= 25) {
+            Alert.alert('Your nickname is too long');
+        } else {
+            console.log("before connection");
+            try {
+              const response = await connect(userId, {
+                nickname: nickName,
+                accessToken: token,
+              });
+              console.log("after successful connection", response);
+            } catch (error) {
+              console.log(error);
+            }
+        }
+    }
+
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+            <TextInput placeholder='Enter User_id'
+                style={style.input}
+                placeholderTextColor={'black'}
+                onChangeText={id => setUserId(id)}
+                defaultValue={userId} />
+
+            <TextInput placeholder='Enter Nickname'
+                style={style.input}
+                placeholderTextColor={'black'}
+                onChangeText={name => setNickName(name)}
+                defaultValue={nickName} />
+
+            <TextInput placeholder='Enter Accesstoken'
+                style={style.input}
+                placeholderTextColor={'black'}
+                onChangeText={access => setToken(access)}
+                defaultValue={token} />
+
             <Pressable
                 style={{
                     width: 120,
@@ -15,7 +59,7 @@ const SignInScreen = () => {
                     alignItems: 'center',
                     justifyContent: 'center',
                 }}
-                onPress={() => connect('USER_ID', { nickname: 'NICKNAME' })}
+                onPress={() => signin()}
             >
                 <Text>{'Sign in'}</Text>
             </Pressable>
@@ -24,3 +68,13 @@ const SignInScreen = () => {
 };
 
 export default SignInScreen;
+
+const style = StyleSheet.create({
+    input: {
+        borderWidth: 1,
+        width: '80%',
+        alignSelf: 'center',
+        marginVertical: 10, 
+        
+    }
+})
